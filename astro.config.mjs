fb1b838a8f834f9d77db1ccda48c9f9ea650ff5c
Mr Roux <XenocodeRCE@users.noreply.github.com>
2024-07-20 22:45:35 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import playformCompress from "@playform/compress";
+import pagefind from "astro-pagefind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.saroprock.com',
+  style: {
+    scss: {
+      includePaths: ['./src/styles']
+    }
+  },
+  integrations: [mdx(), sitemap(), tailwind(), playformCompress(), pagefind()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  },
+});
